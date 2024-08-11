b63842b6a187e189369d72cc3f81f4039fe6d0e4
Theotime Donnenfeld <tdonnenfeld@proton.me>
2024-08-12 00:04:00 +0200

    feat: removed /home

--- app.js
+++ app.js
@@ -7,12 +7,8 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/home", (req, res) => {
-    res.render("home.ejs");
-});
-
 app.get("/", (req, res) => {
-    res.redirect("home");
+    res.render("home.ejs");
 });
 
 app.listen(port, () => {
